f0471674e807f212f288fe88878cf2700cee49f5
Michael Li <36088648+magicalpuffin@users.noreply.github.com>
2023-05-09 20:46:37 -0700

    added invh notebook

--- puffinsystems_homepage/src/routes/notebooks/+page.svelte
+++ puffinsystems_homepage/src/routes/notebooks/+page.svelte
@@ -9,6 +9,16 @@
 <hr>
 
 <Card
+img_src= "/card_images/invh_house_rental_data.png"
+card_title= "INVH Housing Data Analysis ">
+    <p>Invitation Homes (INVH) rents out single family homes. Data was webscraped. ML model with sklearn to predict market_rent</p>
+    <p>jupyter, pandas, plotly, sklearn</p>
+    <ul style="list-style-type: none;">                
+        <li><a href="/notebooks/market_rent_prediciton_preliminary.html">Market Rent Prediction Preliminary</a></li>
+    </ul>
+</Card>
+
+<Card
 img_src= "/card_images/tinywars_ranked_statistics.png"
 card_title= "Tiny Wars Ranked Statistics">
     <p>Tiny Wars is a small online game with a ranked ladder. Cleaned the data to create various data visualizations about ranked matches.</p>
