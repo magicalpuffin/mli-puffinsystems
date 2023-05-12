c4d939dd5057c241714641fcb44619dc90dbe4ef
Michael Li <36088648+magicalpuffin@users.noreply.github.com>
2023-05-11 21:50:00 -0700

    added prettier and tailwind

--- puffinsystems_homepage/src/routes/+layout.svelte
+++ puffinsystems_homepage/src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+    import "../app.css";
     import Navbar  from "$lib/Navbar.svelte";
 </script>
 
--- puffinsystems_homepage/src/routes/+page.svelte
+++ puffinsystems_homepage/src/routes/+page.svelte
@@ -1,2 +1,33 @@
 <h1>Puffin Systems</h1>
 <p>Site currently under development. Created with svelte.</p>
+
+<h1
+  class="text-center text-3xl font-bold underline
+    hover:bg-purple-600 hover:text-white"
+>
+  Hello world!
+</h1>
+
+<div
+  class="mx-auto max-w-sm space-y-2 rounded-xl bg-white px-8 py-8 shadow-lg
+    sm:flex sm:items-center sm:space-x-6 sm:space-y-0 sm:py-4"
+>
+  <img
+    class="mx-auto block h-24 rounded-full
+        sm:mx-0 sm:shrink-0"
+    src=""
+    alt="Woman's Face"
+  />
+  <div class="space-y-2 text-center sm:text-left">
+    <div class="space-y-0.5">
+      <p class="text-lg font-semibold text-black">Erin Lindford</p>
+      <p class="font-medium text-slate-500">Product Engineer</p>
+    </div>
+    <button
+      class="rounded-full border border-purple-200 px-4 py-1 text-sm font-semibold text-purple-600
+      hover:border-transparent hover:bg-purple-600 hover:text-white
+      focus:font-extrabold focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+      >Message</button
+    >
+  </div>
+</div>
