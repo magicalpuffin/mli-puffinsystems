d1066b44dbe3dc5ceb2e9958ba16d467be85c8f3
Michael Li <36088648+magicalpuffin@users.noreply.github.com>
2023-07-02 11:49:30 -0700

    added AboutMe
    reduced resolution

---
+++ puffinsystems_homepage/src/lib/components/AboutMe.svelte
@@ -0,0 +1,21 @@
+<div class="flex max-w-3xl flex-row justify-between rounded-lg shadow-lg">
+  <article class="prose my-4 ml-4 mr-2">
+    <h1 class="my-0">Michael Li</h1>
+    <p class="prose-sm my-2">subtitle goes here</p>
+    <p>
+      Hi, I am still developing this site, this is just some filler text for
+      now.
+    </p>
+    <h3 class="my-0">Bio</h3>
+    <p>Maybe insert a basic bio here</p>
+    <h3 class="my-0">Contact</h3>
+    <p>Maybe insert a contact info here</p>
+    <h3 class="my-0">Resume</h3>
+    <p>Maybe insert some resume content here</p>
+  </article>
+  <img
+    class="my-auto w-1/3 rounded-lg object-cover shadow-lg shadow-black"
+    src={"images/self_portrait.JPG"}
+    alt="self portrait"
+  />
+</div>
--- puffinsystems_homepage/src/routes/+page.svelte
+++ puffinsystems_homepage/src/routes/+page.svelte
@@ -1,16 +1,10 @@
-<div class="flex flex-row">
-  <article class="prose">
-    <h1 class="mb-0">Michael Li</h1>
-    <p class="prose-sm my-2">someone with website</p>
-    <p>Hi, I deploy garabge to this domain and its subdomains. Check it out.</p>
-  </article>
-  <img
-    class="my-auto h-36 rounded-full object-cover shadow-md shadow-black"
-    src={"self_portrait.jpg"}
-    alt="self portrait"
-  />
+<script lang="ts">
+  import AboutMe from "$lib/components/AboutMe.svelte";
+</script>
+
+<div class="flex flex-col">
+  <div class="my-2">
+    <AboutMe />
+  </div>
+  <!-- <AboutMe /> -->
 </div>
-<article class="prose mt-2">
-  <h3>Bio</h3>
-  <p>2023: Not dead.</p>
-</article>
