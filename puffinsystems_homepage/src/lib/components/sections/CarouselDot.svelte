0c3e3a241c8814d89cd188a6245821506c12c827
Michael Li <36088648+magicalpuffin@users.noreply.github.com>
2023-07-11 21:14:14 -0700

    basic carousel implementation

---
+++ puffinsystems_homepage/src/lib/components/sections/CarouselDot.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  export let positionStart = 0;
+  export let positionEnd = 0;
+  export let xLeft = 0;
+
+  const dispatch = createEventDispatcher();
+</script>
+
+<button
+  on:click={() => {
+    dispatch("clickDot", positionStart);
+    // console.log(positionStart);
+    // console.log(positionEnd);
+    // console.log(xLeft);
+  }}
+  class="rounded-full border border-blue-600 px-3 py-3 {xLeft >=
+    positionStart && xLeft < positionEnd
+    ? 'bg-blue-600'
+    : 'bg-white'}"
+/>
