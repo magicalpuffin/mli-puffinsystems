47e92731dd3a39a6962fac91784e16bd2bece728
puffins <36088648+magicalpuffin@users.noreply.github.com>
2023-12-25 20:49:18 -0800

    added vscode pytest, testcase for blogpost

---
+++ markdown_to_json/config.py
@@ -0,0 +1,5 @@
+TEST_PATH = "markdown_to_json/test/data/"
+BLOG_PATH = "puffinsystems_homepage/static/markdown/blog/"
+CARD_PATH = "puffinsystems_homepage/static/markdown/card/"
+
+DATA_PATH = "puffinsystems_homepage/static/data/"
--- markdown_to_json/test/test_utils.py
+++ markdown_to_json/test/test_utils.py
@@ -1,4 +1,8 @@
-from markdown_to_json.utils import get_slug, get_title
+import datetime
+
+from markdown_to_json.config import TEST_PATH
+from markdown_to_json.markdown_types import BlogPost
+from markdown_to_json.utils import get_blog_post, get_slug, get_title
 
 
 class TestGetTitle:
@@ -13,3 +17,16 @@ class TestGetSlug:
         filename = "2023-07-15_demo-manufacturing-kanban.md"
         target = "2023-07-15_demo-manufacturing-kanban"
         assert get_slug(filename) == target
+
+
+class TestGetBlogPost:
+    def test_basic_blog(self):
+        filename = "2023-01-01_test-blog-1.md"
+        target = BlogPost(
+            title=get_title(filename),
+            slug_url=get_slug(filename),
+            content="Test blog 1 content.",
+            date_created=datetime.date(2023, 1, 1),
+            date_updated=datetime.date(2023, 1, 2),
+        )
+        assert get_blog_post(TEST_PATH + filename) == target
