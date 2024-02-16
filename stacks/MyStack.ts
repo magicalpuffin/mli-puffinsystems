f15e88f14d75b203adf9ac3bc71203f8ce384c62
Michael Li <mealted.lemon@gmail.com>
2024-02-15 22:16:19 -0800

    redeployed on mli.puffinsystems.com
    closes #18

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -2,13 +2,15 @@ import { Config, StackContext, SvelteKitSite } from "sst/constructs";
 
 export function SITE({ stack }: StackContext) {
   const DOMAIN_NAME = new Config.Parameter(stack, "DOMAIN_NAME", {
-    value: "puffinsystems.com",
+    value: "mli.puffinsystems.com",
   });
   const site = new SvelteKitSite(stack, "Site", {
     path: "puffinsystems_homepage",
     bind: [DOMAIN_NAME],
     customDomain: {
-      domainName: DOMAIN_NAME.value,
+      domainName: "mli.puffinsystems.com",
+      domainAlias: "www.mli.puffinsystems.com",
+      hostedZone: "puffinsystems.com",
     },
   });
 
