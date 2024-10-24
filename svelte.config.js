cdb31ec365854f4ee7654f3fb2564c14a8456316
Michael Li <mealted.lemon@gmail.com>
2024-10-24 00:37:12 -0700

    added svelte enhanced img, md updated to use enhanced image
    static files folder for blog images updated

--- svelte.config.js
+++ svelte.config.js
@@ -18,8 +18,23 @@ const highlighter = await createHighlighter({
 	],
 });
 
+import { visit } from "unist-util-visit";
+
+function rehypeEnhancedImage() {
+	return (tree) => {
+		visit(tree, "element", (node) => {
+			// Check if the node is an img element
+			if (node.tagName === "img") {
+				// Change the tag name to 'enhanced:img'
+				node.tagName = "enhanced:img";
+			}
+		});
+	};
+}
+
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
+	rehypePlugins: [rehypeEnhancedImage],
 	extensions: [".svx", ".md"],
 	highlight: {
 		highlighter: async (code, lang = "text") => {
