421ee5911558748a0e5ef1822decb5f7bcc97ab2
Michael Li <mealted.lemon@gmail.com>
2024-10-24 20:25:53 -0700

    exclude gif from enhanced image

--- svelte.config.js
+++ svelte.config.js
@@ -20,13 +20,29 @@ const highlighter = await createHighlighter({
 
 import { visit } from "unist-util-visit";
 
-function rehypeEnhancedImage() {
+/**
+ * @import {Root} from 'hast'
+ */
+
+export function rehypeEnhancedImage() {
+	/**
+	 * @param {Root} tree
+	 * @return {undefined}
+	 */
 	return (tree) => {
 		visit(tree, "element", (node) => {
 			// Check if the node is an img element
 			if (node.tagName === "img") {
-				// Change the tag name to 'enhanced:img'
-				node.tagName = "enhanced:img";
+				if (typeof node.properties.src === "string") {
+					const srcext = node.properties.src?.toString().split(".").pop();
+
+					if (srcext === "gif") {
+						node.properties.src = node.properties.src.replace("/static", "");
+					} else {
+						// Change the tag name to 'enhanced:img'
+						node.tagName = "enhanced:img";
+					}
+				}
 			}
 		});
 	};
