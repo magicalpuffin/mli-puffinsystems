d7dda4bdaccabc8b53f0364ba37a2cbc96e25735
Michael Li <mealted.lemon@gmail.com>
2025-03-13 20:25:13 -0700

    switch prerender to true, update dependencies

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -3,7 +3,7 @@ import { error, redirect } from "@sveltejs/kit";
 import type { Component } from "svelte";
 import type { EntryGenerator, PageLoad } from "./$types.js";
 
-export const prerender = "auto";
+export const prerender = true;
 
 export const load = (async ({ params, fetch }) => {
 	const BLOG_SLUG = params.slug;
