8d850ab9dfc852a29ef635d275202d2349676507
Michael Li <mealted.lemon@gmail.com>
2025-12-06 11:00:58 -0800

    updating blog import

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -18,13 +18,13 @@
 >
 
 <Heading1 id="blog">My Blog Posts</Heading1>
-<div class="mt-2 menu bg-base-200 rounded-box">
+<div class="mt-2">
 	{#each data.blogMetadataList as blog}
 		<a
 			href="/blog/{blog.slug}"
 			class="block p-4 border-l-2 border-secondary hover:border-primary hover:bg-secondary/30"
 		>
-			<div class="text-xs text-gray-400">{blog.createdDate.slice(0, 10)}</div>
+			<div class="text-xs text-gray-600">{blog.createdDate.slice(0, 10)}</div>
 			<h3 class="text-xl font-bold">{blog.title}</h3>
 			<p
 				class="p-4 my-2 text-sm border hover:shadow-xl border-primary duration-600"
--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -4,17 +4,23 @@ import type { PageLoad } from "./$types";
 export const prerender = true;
 
 export const load = (async () => {
-	const modules = import.meta.glob("/src/lib/content/blog/*.md") as BlogModules;
-	let blogMetadataList: BlogMetadata[] = [];
+	const modules = import.meta.glob("/src/lib/content/blog/*.md", {
+		eager: true,
+	}) as BlogModules;
+	// let blogMetadataList: BlogMetadata[] = [];
 
-	for (const path of Object.keys(modules)) {
-		await modules[path]().then((mod) => {
-			blogMetadataList.push(mod.metadata);
-		});
-	}
-	blogMetadataList = blogMetadataList.sort((a, b) =>
-		b.createdDate.localeCompare(a.createdDate),
-	);
+	const blogMetadataList = Object.values(modules)
+		.map((mod: any) => mod.metadata)
+		.sort((a, b) => b.createdDate.localeCompare(a.createdDate));
+
+	// for (const path of Object.keys(modules)) {
+	// 	await modules[path]().then((mod) => {
+	// 		blogMetadataList.push(mod.metadata);
+	// 	});
+	// }
+	// blogMetadataList = blogMetadataList.sort((a, b) =>
+	// 	b.createdDate.localeCompare(a.createdDate),
+	// );
 
 	return { blogMetadataList };
 }) satisfies PageLoad;
